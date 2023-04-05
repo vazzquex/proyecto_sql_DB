daecaa21adfc94b4c10fcc3895fea22a715d3df8
Santiago Vázquez <108772623+Vazzquex2402@users.noreply.github.com>
2023-04-05 20:49:41 -0300

    Delete funciones directory

--- funciones/NombreClienteID.sql
+++
@@ -1,9 +0,0 @@
-CREATE DEFINER=`root`@`localhost` FUNCTION `nombre_cliente_id`(Usuario_ID INT) 
-	RETURNS varchar(100) 
-    CHARSET utf8mb4
-    DETERMINISTIC
-BEGIN
-	DECLARE nombre_completo VARCHAR(100);
-	SELECT CONCAT(nombre, ' ', apellido) INTO nombre_completo FROM cliente WHERE Usuario_ID = cliente_id;
-	RETURN nombre_completo;
-END
